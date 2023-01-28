4965fe05252aee38c6aba3b9bc10d97772918405
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-28 22:34:08 +0100

    completed🎉

--- task_0/flexbox/src/App/App.js
+++ task_0/flexbox/src/App/App.js
@@ -1,10 +1,14 @@
-import '../styles/App.css';
 import Heading from '../header/head';
+import Main from '../body/body';
+import Footer from '../footer/footer';
+import '../styles/main.css'
 
 function App() {
   return (
     <div className="App">
       <Heading />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- task_0/flexbox/src/header/head.jsx
+++ task_0/flexbox/src/header/head.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {Component} from "react";
-import blackLogo from '../images/logo-black.png'
+import whiteLogo from '../images/logo-white.png'
 import '../styles/main.css';
 
 export default class Heading extends Component {
@@ -10,37 +10,39 @@ export default class Heading extends Component {
             <div className="header">
                 <header>
                     <div className="container">
-                        <div className="header-logo">
-                        <a href="#">
-                            <img src={ blackLogo } alt="Techium logo" style={{width: '160', height: '40'}} />
-                        </a>
-                        </div>
+                        <div className="header-container">
+                            <div className="header-logo">
+                                <a href="#">
+                                    <img src={ whiteLogo } alt="Techium logo" style={{width: '160', height: '40'}} />
+                                </a>
+                            </div>
 
-                        <nav className="navbar-menu">
-                            <ul className="nav">
-                                <li className="nav-item">
-                                    <a href="#" className="nav-link">Home</a>
-                                </li>
-                                <li className="nav-item section-services">
-                                    <a href="#services" className="nav-link">Services</a>
-                                </li>
-                                <li className="nav-item section-works">
-                                    <a href="#works" className="nav-link">Works</a>
-                                </li>
-                                <li className="nav-item section-about-us">
-                                    <a href="#about" className="nav-link">About</a>
-                                </li>
-                                <li className="nav-item section-latest-news">
-                                    <a href="#latest_news" className="nav-link">Latest news</a>
-                                </li>
-                                <li className="nav-item section-testimonial">
-                                    <a href="#testimonials" className="nav-link">Testimonials</a>
-                                </li>
-                                <li className="nav-item section-contact">
-                                    <a href="#contact" className="nav-link">Contact</a>
-                                </li>
-                            </ul>
-                        </nav>
+                            <nav className="navbar-menu">
+                                <ul className="nav">
+                                    <li className="nav-item">
+                                        <a href="#" className="nav-link">Home</a>
+                                    </li>
+                                    <li className="nav-item section-services">
+                                        <a href="#services" className="nav-link">Services</a>
+                                    </li>
+                                    <li className="nav-item section-works">
+                                        <a href="#works" className="nav-link">Works</a>
+                                    </li>
+                                    <li className="nav-item section-about-us">
+                                        <a href="#about" className="nav-link">About</a>
+                                    </li>
+                                    <li className="nav-item section-latest-news">
+                                        <a href="#latest_news" className="nav-link">Latest news</a>
+                                    </li>
+                                    <li className="nav-item section-testimonial">
+                                        <a href="#testimonials" className="nav-link">Testimonials</a>
+                                    </li>
+                                    <li className="nav-item section-contact">
+                                        <a href="#contact" className="nav-link">Contact</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </header>
             </div>
